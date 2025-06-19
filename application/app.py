b32b46431ca79fcd3e1a5a79c0f2c92f6265db55
abhishekindra <abhishekchaurasia@va2pt.com>
2025-06-19 19:05:36 +0530

    About Lens Corporation

--- application/app.py
+++ application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Dummy Python Application!"
+    return "Lens Corporation stands as a trusted and innovative leader in its industry, consistently delivering high-quality products and exceptional service. With a strong commitment to excellence, integrity, and customer satisfaction, Lens Corporation continues to set new standards and build lasting relationships with clients and partners worldwide."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
