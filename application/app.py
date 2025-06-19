5c5b025da5f914f16d521054c717dfbfcdc55cbe
abhishekindra <abhishekchaurasia@va2pt.com>
2025-06-19 19:09:02 +0530

    About Lens Corporation

--- application/app.py
+++ application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Lens Corporation stands as a trusted and innovative leader in its industry, consistently delivering high-quality products and exceptional service. With a strong commitment to excellence, integrity, and customer satisfaction, Lens Corporation continues to set new standards and build lasting relationships with clients and partners worldwide."
+    return "Lens Corporation is at the forefront of biometrics and AI, building state‑of‑the‑art identification solutions—from face, fingerprint, and iris recognition to cross‑media translation and 3D asset generation—that run efficiently on edge devices, even offline lenscorp.ai. Founded in 2016 in Lansing, Michigan, and now with a growing presence in Gurugram, Lens is driven by a passionate, academically backed team of researchers and engineers, committed to ethical, transparent, and explainable AI By delivering custom, in‑house AI‑powered solutions—including intelligent image analysis, voice and image translation, and realistic 3D modeling—they empower businesses to focus on innovation, not implementation With this dynamic expertise and a global mindset, Lens Corporation doesn’t just meet today’s demands—they build tomorrow’s vision, today."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
